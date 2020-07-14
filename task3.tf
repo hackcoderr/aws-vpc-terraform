5963a406dcc7814baf06088a83f4b1f06b21a988
Sachin Kumar <56272723+hackcoderr@users.noreply.github.com>
2020-07-14 10:48:08 +0530

    Update task3.tf
    this is my updated task3.tf file

--- task3.tf
+++ task3.tf
@@ -1,8 +1,8 @@
 // Provide Credentials
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAJLRH3UXU3H23ELQQ"
-  secret_key = "UqAAQIxrY+MkA3PAFtMAVFEBg6ug9mRlK4Z0Pr1/"
+  access_key = "your_access_key"
+  secret_key = "your_secret_key"
   profile = "sachin"
 }
 
